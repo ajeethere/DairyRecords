3f9df3ae83fa26a99232c0d0c59cf59ee092f94d
ajeethere <ajeetgathala@gmail.com>
2018-11-23 13:58:35 +0530

    database updated with two tables

--- app/src/main/java/com/example/ajeet/dairyrecords/DairyDBHelper.java
+++ app/src/main/java/com/example/ajeet/dairyrecords/DairyDBHelper.java
@@ -14,39 +14,44 @@ public class DairyDBHelper extends SQLiteOpenHelper {
     public static final String Col_Add="Address";
     public static final String Col_Phone="Phone";
 
-    public static final String TABLE_SOLED_ITEMS="Sold Item List";
-    public static final String Col_Buyer_Name="Buyer";
+    public static final String TABLE_SOLED_ITEMS="SoldItemList";
     public static final String Col_Product_Serial_No="ProductSerialNumber";
+    public static final String Col_Buyer_Name="Buyer";
     public static final String Col_Product_Name="ProductName";
     public static final String Col_Price="Price";
     public static final String Col_Quantity="ProductsQuantity";
-    public static final String Col_TotalAmount="TotalAmount";
+    public static final String Col_Total_Amount="TotalAmount";
     public static final String Col_Total_Due_Amount="TotalDueAmount";
 
 
-
-
     public DairyDBHelper(Context context) {
-        super(context, DATABASE_NAME, null, 1);
+        super(context, DATABASE_NAME, null, 2);
         SQLiteDatabase db=this.getWritableDatabase();
 
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("create table " + TABLE_BUYERS +"(" + Col_SerialNo + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+        db.execSQL("create table " + TABLE_BUYERS + "( " + Col_SerialNo + " INTEGER PRIMARY KEY AUTOINCREMENT, "
                 + Col_Name + " TEXT,"+ Col_SurName + " TEXT,"
                 + Col_Fathers_Name +" TEXT, "+Col_Add+" TEXT, "+ Col_Phone + " INTEGER)");
-        db.execSQL("create table " + TABLE_SOLED_ITEMS +"(" + Col_Product_Serial_No + " INTEGER PRIMARY KEY AUTOINCREMENT,"
-                + Col_Buyer_Name + " TEXT, " + Col_Product_Name + " TEXT, " + Col_Price + " INTEGER, " + Col_Quantity + " INTEGER, "
-                + Col_TotalAmount + " INTEGER, " + Col_Total_Due_Amount + " INTEGERf)");
+
+        db.execSQL("create table " + TABLE_SOLED_ITEMS + "( " + Col_Product_Serial_No + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + Col_Buyer_Name + " TEXT,"+ Col_Product_Name + " TEXT,"
+                + Col_Price +" INTEGER, "+Col_Quantity+" INTEGER, "+ Col_Total_Amount + " INTEGER,"+ Col_Total_Due_Amount + " INTEGER)");
+
+//        db.execSQL("create table " + TABLE_SOLED_ITEMS +"(" + Col_Product_Serial_No + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+//                + Col_Buyer_Name + "TEXT, "+ Col_Product_Name +"TEXT, " + Col_Price + "INTEGER," + Col_Quantity + "INTEGER," + Col_Total_Amount + "INTEGER,"
+//                + Col_Total_Due_Amount + "INTEGER)");
+
+
 
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("DROP TABLE IF EXISTS "+TABLE_BUYERS+TABLE_SOLED_ITEMS);
-       // db.execSQL("DROP TABLE IF EXISTS "+TABLE_SOLED_ITEMS);
+        db.execSQL("DROP TABLE IF EXISTS "+TABLE_BUYERS);
+        db.execSQL("DROP TABLE IF EXISTS "+TABLE_SOLED_ITEMS);
         onCreate(db);
     }
 }
---
+++ app/src/main/java/com/example/ajeet/dairyrecords/NewBuyerFragment.java
@@ -0,0 +1,31 @@
+package com.example.ajeet.dairyrecords;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.ajeet.dairyrecords.R;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class NewBuyerFragment extends Fragment {
+
+
+    public NewBuyerFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_new_buyer, container, false);
+    }
+
+}
